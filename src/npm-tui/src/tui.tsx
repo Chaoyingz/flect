5e4febd5eca7d682d7036a9b8ccf9634d6c7f1e1
Chaoying <chaunceywe@gmail.com>
2024-01-13 17:08:17 +0800

    Fix root url

--- src/npm-tui/src/tui.tsx
+++ src/npm-tui/src/tui.tsx
@@ -1,16 +1,25 @@
 import { AnyComponentList, ComponentProps } from "./components/any-component";
 import { useEffect, useState } from "react";
 
-export function Tui() {
-    return <Fetch />;
+type TuiProps = {
+    rootUrl: string;
+};
+
+export function Tui({rootUrl = "/api"}: TuiProps
+) {
+    return <Fetch url={rootUrl} />;
 }
 
-function Fetch() {
+type FetchProps = {
+    url: string;
+};
+
+function Fetch({ url }: FetchProps) {
     const [propsList, setPropsList] = useState<ComponentProps[]>([]);
 
     useEffect(() => {
         async function fetchComponents() {
-            const response = await fetch("https://tui-w8c1.onrender.com/api/");
+            const response = await fetch(url);
             const data = await response.json();
             console.log(data);
             setPropsList(data);
--- src/npm-tui/vite.config.ts
+++ src/npm-tui/vite.config.ts
@@ -2,13 +2,26 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+const serverConfig = {
+    host: true,
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:8000',
+    },
+  }
+
+export default defineConfig(
+
+    {
+
     plugins: [react()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
+        server: serverConfig,
+    preview: serverConfig,
     build: {
         rollupOptions: {
             output: {
