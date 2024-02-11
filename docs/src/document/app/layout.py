e90107f797a1398f9849d6a329268dd0824a5cd6
Chaoying <chaunceywe@gmail.com>
2024-02-11 12:28:23 +0800

    Fix docs header style

--- docs/src/document/app/layout.py
+++ docs/src/document/app/layout.py
@@ -7,52 +7,57 @@ async def layout(outlet: c.AnyComponent = c.Outlet()) -> c.AnyComponent:
         children=[
             c.Container(
                 tag="header",
-                class_name="flex w-full justify-between container h-14 border-b items-center text-sm",
+                class_name="h-14 border-b text-sm",
                 children=[
                     c.Container(
-                        tag="nav",
-                        class_name="flex items-center gap-6",
+                        class_name="flex w-full justify-between container items-center h-full",
                         children=[
-                            c.Link(
-                                href="/",
+                            c.Container(
+                                tag="nav",
+                                class_name="flex items-center gap-6",
                                 children=[
-                                    c.Text(
-                                        text="tui",
-                                        class_name="font-medium text-xl",
-                                    )
+                                    c.Link(
+                                        href="/",
+                                        children=[
+                                            c.Text(
+                                                text="tui",
+                                                class_name="font-medium text-xl",
+                                            )
+                                        ],
+                                    ),
+                                    c.Link(
+                                        href="/docs/",
+                                        children=[
+                                            c.Text(
+                                                text="Docs",
+                                            )
+                                        ],
+                                    ),
+                                    c.Link(
+                                        href="/docs/components/avatar/",
+                                        children=[
+                                            c.Text(
+                                                text="Components",
+                                            )
+                                        ],
+                                    ),
                                 ],
                             ),
-                            c.Link(
-                                href="/docs/",
+                            c.Container(
+                                tag="div",
                                 children=[
-                                    c.Text(
-                                        text="Docs",
-                                    )
-                                ],
-                            ),
-                            c.Link(
-                                href="/docs/components/avatar/",
-                                children=[
-                                    c.Text(
-                                        text="Components",
+                                    c.Link(
+                                        href="https://github.com/Chaoyingz/tui",
+                                        children=[
+                                            c.Text(
+                                                text="GitHub",
+                                            )
+                                        ],
                                     )
                                 ],
                             ),
                         ],
-                    ),
-                    c.Container(
-                        tag="div",
-                        children=[
-                            c.Link(
-                                href="https://github.com/Chaoyingz/tui",
-                                children=[
-                                    c.Text(
-                                        text="GitHub",
-                                    )
-                                ],
-                            )
-                        ],
-                    ),
+                    )
                 ],
             ),
             c.Container(
