a9047a1a11607313c320e80a8094951ca84e0545
Chaoying <chaunceywe@gmail.com>
2024-02-14 16:59:57 +0800

    Fix packaging error

--- src/python-tui/src/tui/__init__.py
+++ src/python-tui/src/tui/__init__.py
@@ -1,6 +1,6 @@
 from tui.application import Tui
 from tui.response import Meta, Response, TitleTemplate
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __all__ = ["Tui", "Response", "Meta", "TitleTemplate"]
