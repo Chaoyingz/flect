bf59669acde5dd0a873b80fcad91434bac596dc3
Chaoying <chaunceywe@gmail.com>
2024-03-03 17:00:16 +0800

    Add docs sitemap

--- docs/src/documentation/app/group__docs/docs/dynamic__content_name/page.py
+++ docs/src/documentation/app/group__docs/docs/dynamic__content_name/page.py
@@ -4,8 +4,21 @@ from fastapi import Path
 from starlette.concurrency import run_in_threadpool
 from tui import PageResponse
 from tui import components as c
+from tui.sitemap import Sitemap
 
 from documentation.utils import get_markdown_content
+
+
+async def sitemap(dynamic_url: str) -> list[Sitemap]:
+    return [
+        Sitemap(
+            url=dynamic_url.format(content_name=content_name),
+            last_modified=None,
+            change_frequency=None,
+            priority=None,
+        )
+        for content_name in ["introduction", "installation", "project-structure"]
+    ]
 
 
 async def page(
