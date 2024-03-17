fe10013835a2e795419079b5f19947882e644a05
Chaoying <chaunceywe@gmail.com>
2024-03-17 15:46:07 +0800

    Update docs version

--- docs/docs-ui/vite.config.ts
+++ docs/docs-ui/vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { splitVendorChunkPlugin } from "vite";
 
 const serverConfig = {
   host: true,
@@ -12,7 +11,7 @@ const serverConfig = {
 };
 
 export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
